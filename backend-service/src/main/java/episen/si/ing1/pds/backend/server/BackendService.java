8fecd8ced27f5825b9d7a743f3510ead14c64d23
Mehdi Koumad <koumad094mehdi@gmail.com>
2021-01-29 14:32:14 +0100

    Add logger for class BackendService and Client

--- backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
+++ backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
@@ -1,5 +1,13 @@
 package episen.si.ing1.pds.backend.server;
 
-public class BackendService {
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackendService {
+    private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
+    public static void main(String[] args) {
+        logger.info("Backend service is running!");
+
+    }
 }
---
+++ client/src/main/java/episen/si/ing1/pds/client/Client.java
@@ -0,0 +1,11 @@
+package episen.si.ing1.pds.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private final static Logger logger = LoggerFactory.getLogger(Client.class.getName());
+    public static void main(String[] args) {
+        logger.info("Client service is running!");
+    }
+}
