4b86a539b826a49d03d677aca21cc8ec57ddb7dd
Mehdi Koumad <koumad094mehdi@gmail.com>
2021-01-29 14:56:25 +0100

    Last commit on master branch

--- backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
+++ backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
@@ -1,13 +1,8 @@
 package episen.si.ing1.pds.backend.server;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BackendService {
-    private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
     public static void main(String[] args) {
-        logger.info("Backend service is running!");
+        System.out.println("BackendService is running");
 
     }
 }
--- client/src/main/java/episen/si/ing1/pds/client/Client.java
+++ client/src/main/java/episen/si/ing1/pds/client/Client.java
@@ -1,11 +1,7 @@
 package episen.si.ing1.pds.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Client {
-    private final static Logger logger = LoggerFactory.getLogger(Client.class.getName());
     public static void main(String[] args) {
-        logger.info("Client service is running!");
+        System.out.println("Client is running");
     }
 }
