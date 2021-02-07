b423b5e0b6366ad0852c97bccd6c836c2e11fe6c
AcilKh <acilpro@outlook.fr>
2021-02-07 18:57:29 +0100

    Adding the word Great before the word Backend

--- backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
+++ backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class BackendService {
     private static final Logger serverLogger = LoggerFactory.getLogger(BackendService.class.getName());
     public static void main(String[] args) {
-        serverLogger.info("BackendService is running");
+        serverLogger.info(" Great : BackendService is running");
         System.out.println("mvn install with cmd");
         System.out.println("");
     }
