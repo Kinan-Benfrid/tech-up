d091fd3f7a69ab404ff3853cef5f9bda5100e63e
Kinan-Benfrid <kinan.benfridpro@gmail.com>
2021-02-07 20:02:00 +0100

    add options for client.bat

--- client/src/main/java/episen/si/ing1/pds/client/Client.java
+++ client/src/main/java/episen/si/ing1/pds/client/Client.java
@@ -1,10 +1,23 @@
 package episen.si.ing1.pds.client;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Client {
     private static final Logger clientLogger = LoggerFactory.getLogger(Client.class.getName());
-    public static void main(String[] args) {
-        clientLogger.info("Client is running");
+    public static void main(String[] args) throws ParseException {
+        final Options options = new Options();
+        final Option testMode = Option.builder().longOpt("testMode").build();
+        options.addOption(testMode);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine commandLine = parser.parse(options, args);
+
+        boolean inTestMode = false;
+        if (commandLine.hasOption("testMode")) {
+            inTestMode = true;
+        }
+        clientLogger.info("Client is running(testMode={}).",inTestMode);
     }
+
 }
