0c80767fb929b959c903d8f60ebdf78fdbcad33b
Kinan-Benfrid <kinan.benfridpro@gmail.com>
2021-02-07 18:56:44 +0100

    sans sys

--- backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
+++ backend-service/src/main/java/episen/si/ing1/pds/backend/server/BackendService.java
@@ -1,13 +1,25 @@
 package episen.si.ing1.pds.backend.server;
 
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
+
 public class BackendService {
     private static final Logger serverLogger = LoggerFactory.getLogger(BackendService.class.getName());
-    public static void main(String[] args) {
-        serverLogger.info("BackendService is running");
-        System.out.println("mvn install with cmd");
-        System.out.println("");
+    public static void main(String[] args) throws Exception {
+        final Options options = new Options();
+        final Option testMode = Option.builder().longOpt("testMode").build();
+        options.addOption(testMode);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine commandLine = parser.parse(options, args);
+
+        boolean inTestMode = false;
+        if (commandLine.hasOption("testMode")) {
+            inTestMode = true;
+        }
+        serverLogger.info("BackendService is running (testMode={}).",inTestMode);
     }
 }
