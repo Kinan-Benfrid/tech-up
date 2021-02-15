0d46b3df1f5ec488ecc70ea05ff6c707e2ea13d8
mehdik94 <koumad094mehdi@gmail.com>
2021-02-15 15:23:43 +0100

    add comment

--- client/src/main/java/episen/si/ing1/pds/client/Client.java
+++ client/src/main/java/episen/si/ing1/pds/client/Client.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Client {
-    private static final Logger clientLogger = LoggerFactory.getLogger(Client.class.getName());
+    private static final Logger clientLogger = LoggerFactory.getLogger(Client.class.getName()); // we don't use classic logger
     public static void main(String[] args) throws Exception {
         final Options options = new Options();
         final Option testMode = Option.builder().longOpt("testMode").build();
