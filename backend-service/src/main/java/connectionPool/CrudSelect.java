c6f3bb7113b75194fe388aad32a164df0f6003db
Stephanie Ndongmo <nguimdofany@mail.com>
2021-02-27 20:47:43 +0100

    add of the CrudSelect class

--- backend-service/src/main/java/connectionPool/CrudSelect.java
+++ backend-service/src/main/java/connectionPool/CrudSelect.java
@@ -8,45 +8,45 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Test {
+class CrudSelect {
     private Connection c;
 
 
-    public void fonctionSelect() throws SQLException {
+    public void functionSelect() throws SQLException {
 
         try{
             Statement stmt = null;
 
             stmt = c.createStatement();
             ResultSet rs;
-            rs = stmt.executeQuery("SELECT * FROM Etudiants ");
+            rs = stmt.executeQuery("SELECT Firstname FROM Etudiant");
 
             while (true) {
 
                 if (!rs.next()) break;
                 {
-                    String name = rs.getString("nom");
-                    String surname = rs.getString("prenom");
-                    int idUser = rs.getInt("id");
+
+                    String name = rs.getString("Firstname");
+                    //String surname = rs.getString("Lastname");
+                    //int idUser = rs.getInt("id");
+                   // System.out.println(idUser );
                     System.out.println(name );
-                    System.out.println(surname);
-                    System.out.println(idUser );
+                    //System.out.println(surname);
+
                 }
-                c.closeConnection();
+                c.close();
             }
         } catch (Exception e) {
             System.err.println("Got an exception! ");
             System.err.println(e.getMessage());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
 
     }
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Test t = new Test();
+        CrudSelect t = new CrudSelect();
         JDBCConnectionPool j = new JDBCConnectionPool();
         DataSource ds = new DataSource();
         t.c= ds.receiveConnection();
-        t.c.fonctionSelect();
+        t.functionSelect();
     }
 }
