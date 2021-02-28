0753099d0ed8127e9d8b5964f827bee59817116f
Kinan-Benfrid <kinan.benfridpro@gmail.com>
2021-02-28 16:39:32 +0100

    insert function and constructor

--- backend-service/src/main/java/connectionPool/Test.java
+++ backend-service/src/main/java/connectionPool/Test.java
@@ -3,6 +3,7 @@ package connectionPool;
 
 
 import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +11,25 @@ import java.sql.Statement;
 
 public class Test {
     private Connection c;
+    private Statement st;
+    private DataSource ds;
 
+    public Test() throws SQLException, ClassNotFoundException {
+
+        ds= new DataSource();
+        c=ds.receiveConnection();
+        st=c.createStatement();
+    }
+    public void Insert() throws SQLException { //insert function
+        try {
+            st = c.createStatement();
+            int i = st.executeUpdate("INSERT INTO Etudiant (firstname, lastname) VALUES ('?', '?' )");
+            System.out.println("result of the request : " + i + " lines added");
+        } catch (SQLException e) {
+            System.err.println("error");
+            System.err.println(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Test t = new Test();
@@ -18,12 +37,13 @@ public class Test {
         DataSource ds = new DataSource();
         t.c= ds.receiveConnection();
         Statement st = t.c.createStatement();
-        ResultSet rs = st.executeQuery("select * from Etudiant");
+        ResultSet rs = st.executeQuery("select * from Etudiant"); // sql request for test
         while (rs.next()){
             String firstname = rs.getString(2);
             String lastname = rs.getString(3);
             System.out.println(firstname);
             System.out.println(lastname);
         }
+        t.Insert();
     }
 }
