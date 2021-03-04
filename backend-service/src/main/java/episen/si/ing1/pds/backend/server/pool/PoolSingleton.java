e4079a9d990b073fd24eb6226ddb0696ef4a9111
Mehdi Koumad <koumad094mehdi@gmail.com>
2021-03-04 10:36:08 +0100

    Adding comments

--- backend-service/src/main/java/episen/si/ing1/pds/backend/server/pool/PoolSingleton.java
+++ backend-service/src/main/java/episen/si/ing1/pds/backend/server/pool/PoolSingleton.java
@@ -1,6 +1,7 @@
 package episen.si.ing1.pds.backend.server.pool;
 
 public enum PoolSingleton {
+    // an instance of JDBCConnectionPool
     Instance;
     JDBCConnectionPool jdbcConnectionPool;
     PoolSingleton(){
