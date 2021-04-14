65288b9c425c050e08ab1cbe087f8b9b75a53f15
Stephanie Ndongmo <nguimdofany@mail.com>
2021-04-14 21:16:31 +0200

    Creation of PageDeConfig.This package content my code

---
+++ client/src/main/java/episen/si/ing1/pds/client/ConfigFenetreElectro/PageDeConfig.java
@@ -0,0 +1,16 @@
+package episen.si.ing1.pds.client.ConfigFenetreElectro;
+
+import javax.swing.*;
+
+public class PageDeConfig extends JFrame {
+    public static void main(String[] args){
+
+
+
+        JFrame fenetre = new JFrame("ConfigurationWindow");
+        fenetre.setSize(400,400);
+        fenetre.setVisible(true);
+        fenetre.setResizable(true);
+
+    }
+}
