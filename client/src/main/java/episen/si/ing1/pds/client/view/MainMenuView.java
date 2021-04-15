8f1b33c91445451b7080230c5cf43123b6d78b7f
Stephanie Ndongmo <nguimdofany@mail.com>
2021-04-16 00:04:28 +0200

    Modification of the Frame color

--- client/src/main/java/episen/si/ing1/pds/client/view/MainMenuView.java
+++ client/src/main/java/episen/si/ing1/pds/client/view/MainMenuView.java
@@ -16,33 +16,48 @@
         label.setText("Tech-up");
         label.setFont(new Font("Tech-up", Font.PLAIN,20));*/
 
-            JButton b = new JButton("Configurer");
-            JButton b1 = new JButton("Louer");
-            JButton b2 = new JButton("Afficher les espaces");
-            b.setBounds(300,150,200,50);
-            b1.setBounds(300,300,200,50);
-            b2.setBounds(300,450,200,50);
-            //b.setSize(200,50);
-            //b1.setSize(200,50);
-            //b2.setSize(200,50);
-
+        JLabel label = new JLabel("TECH-UP");
+        JButton b = new JButton("Configurer");
+        JButton b1 = new JButton("Louer");
+        JButton b2 = new JButton("Afficher les espaces");
+        b.setBounds(200,100,200,50);
+        b1.setBounds(200,250,200,50);
+        b2.setBounds(200,400,200,50);
+        b.setBackground(Color.green);
+            b1.setBackground(Color.lightGray);
+        b2.setBackground(Color.green);
 
         JPanel panH = new JPanel();
         JPanel panb= new JPanel();
         JPanel pan= new JPanel();
+        JPanel container = new JPanel();
 
-        panH.setBackground(Color.green);
-        panb.setBackground(Color.green);
-        pan.setBackground(Color.BLACK);
+        panH.setBackground(Color.orange);
+        panb.setBackground(Color.orange);
+        //pan.setBackground(Color.lightGray);
+            container.setBackground(Color.white);
+
 
         panH.setPreferredSize(new Dimension(50,50));
         panb.setPreferredSize(new Dimension(50,50));
 
-
         panH.setLayout(new BorderLayout());
         panb.setLayout(new BorderLayout());
-        pan.setLayout(new BorderLayout());
 
+       // pan.setLayout(new BorderLayout());
+            pan.add(b);
+            pan.add(b1);
+            pan.add(b2);
+        container.setLayout(new BorderLayout());
+        container.add(pan, BorderLayout.SOUTH);
+
+            /*Font police = new Font("Tahoma", Font.BOLD, 16);
+            label.setFont(police);
+            label.setForeground(Color.blue);
+            label.setHorizontalAlignment(JLabel.CENTER);
+            label.setFont(new Font("green", Font.PLAIN,20));
+            //panH.add(label, BorderLayout.NORTH);
+*/
 
         JFrame f = new JFrame("Tech-up");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,9 +69,13 @@
         f.getContentPane().add(b);
         f.getContentPane().add(b1);
         f.getContentPane().add(b2);
+
         f.add(panH, BorderLayout.NORTH);
+        //f.add(label, BorderLayout.NORTH);
         f.add(panb, BorderLayout.SOUTH);
         f.add(pan, BorderLayout.CENTER);
+        f.setLocationRelativeTo(null);
+        f.setResizable(false);
 
 
 
