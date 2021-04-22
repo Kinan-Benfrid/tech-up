35d1ff7fdbbd255e42dab6621e32fc12312d0326
Stephanie Ndongmo <nguimdofany@mail.com>
2021-04-22 16:57:05 +0200

    Addition of the table that displays the current state of the window

--- client/src/main/java/episen/si/ing1/pds/client/view/PageOfConfigWindow.java
+++ client/src/main/java/episen/si/ing1/pds/client/view/PageOfConfigWindow.java
@@ -1,38 +1,35 @@
 package episen.si.ing1.pds.client.view;
-
-
 import javax.swing.*;
 import java.awt.*;
 
 public class PageOfConfigWindow extends JFrame {
     public static void main(String[] args){
+        Object[][] donnees = {
+                {"Temperature interieure de la piece",10},
+                {"Temperature exterieure de la piece", 20},
+                {"Pourcentage des stores", 25},
+                {"Luminosite de la piece", 0 + "(eteinte)"},
+                {"Luminosite exterieure", 50},
+                {"Position du soleil", "Nord-Oeust"},
+                {"Pourcentage de la teinte", 0 + "%"},
+        };
+        String[] entetes = {"Facteur d'influence sur la fenetre", "Valeur"};
+        JTable tableau = new JTable(donnees, entetes);
 
 
         JButton b = new JButton("CONFIGURATION DES FENETRES ELECTRO-CHROMATIQUES");
-
-
         b.setBounds(10,40,560,50);
-
         b.setBackground(Color.cyan);
-
-
         JPanel panH = new JPanel();
         JPanel panb= new JPanel();
-        JPanel pan= new JPanel();
-
 
         panH.setBackground(Color.orange);
         panb.setBackground(Color.orange);
-        pan.setBackground(Color.lightGray);
-
-
-
         panH.setPreferredSize(new Dimension(30,30));
         panb.setPreferredSize(new Dimension(30,30));
 
         panH.setLayout(new BorderLayout());
         panb.setLayout(new BorderLayout());
-        pan.setLayout(new BorderLayout());
 
 
         JFrame f = new JFrame("Tech-up");
@@ -43,13 +40,13 @@ public class PageOfConfigWindow extends JFrame {
         f.setResizable(true);
         f.setLocationRelativeTo(null);
         f.setResizable(false);
-
         f.getContentPane().add(b);
-
+        f.add(tableau);
+        f.getContentPane().add(tableau.getTableHeader(), BorderLayout.SOUTH);
+        f.getContentPane().add(tableau, BorderLayout.CENTER);
 
         f.add(panH, BorderLayout.NORTH);
         f.add(panb, BorderLayout.SOUTH);
-        f.add(pan, BorderLayout.CENTER);
 
 
 
