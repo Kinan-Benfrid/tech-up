3388f829040b8454f1526a1a252e443b06ded68c
Stephanie Ndongmo <nguimdofany@mail.com>
2021-04-23 15:24:04 +0200

    change

--- client/src/main/java/episen/si/ing1/pds/client/view/PageOfConfigWindow.java
+++ client/src/main/java/episen/si/ing1/pds/client/view/PageOfConfigWindow.java
@@ -2,9 +2,23 @@ package episen.si.ing1.pds.client.view;
 import javax.swing.*;
 import java.awt.*;
 
-public class PageOfConfigWindow extends CommonFrame {
-    public static void main(String[] args){
-        Object[][] donnees = {
+public class PageOfConfigWindow extends CommonFrame{
+    JPanel pan;
+    JButton b;
+    JTable tableau;
+    public PageOfConfigWindow(){
+
+        pan= new JPanel();
+        pan.setLayout(null);
+        b = new JButton("CONFIGURATION DES FENETRES ELECTRO-CHROMATIQUES");
+        b.setBounds(100,110,560,50);
+        b.setBackground(Color.cyan);
+        pan.add(b);
+        this.add(pan);
+        this.getContentPane().add(b);
+
+/*
+       Object[][] donnees = {
                 {"Temperature interieure de la piece",10},
                 {"Temperature exterieure de la piece", 20},
                 {"Pourcentage des stores", 25},
@@ -14,64 +28,20 @@ public class PageOfConfigWindow extends CommonFrame {
                 {"Pourcentage de la teinte", 0 + "%"},
         };
         String[] entetes = {"Facteur d'influence sur la fenetre", "Valeur"};
-        JTable tableau = new JTable(donnees, entetes);
+        tableau = new JTable(donnees, entetes);
+        this.add(tableau);
+        this.getContentPane().add(tableau.getTableHeader(), BorderLayout.NORTH);
+        this.getContentPane().add(tableau, BorderLayout.CENTER);
 
         JScrollPane scrollPane = new JScrollPane(tableau);
         scrollPane.getViewport().setViewPosition(new Point(100,100));
+        this.add(scrollPane, BorderLayout.SOUTH);
+       */
+    }
+    public static void main(String[] args) {
+        CommonFrame c = new CommonFrame();
+        c.setVisible(true);
 
-        JButton b = new JButton("CONFIGURATION DES FENETRES ELECTRO-CHROMATIQUES");
-        b.setBounds(10,300,560,50);
-        b.setBackground(Color.cyan);
-        JPanel panH = new JPanel();
-        JPanel panb= new JPanel();
-       // JPanel pan= new JPanel();
-
-        panH.setBackground(Color.orange);
-        panb.setBackground(Color.orange);
-        panH.setPreferredSize(new Dimension(30,30));
-        panb.setPreferredSize(new Dimension(30,30));
-
-        panH.setLayout(new BorderLayout());
-        panb.setLayout(new BorderLayout());
-
-
-
-
-        JFrame f = new JFrame("Tech-up");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setLayout(new BorderLayout());
-        f.setSize(600,600);
-        f.setVisible(true);
-        f.setResizable(true);
-        f.setLocationRelativeTo(null);
-        f.setResizable(false);
-        f.getContentPane().add(b);
-        //f.add(pan);
-
-        f.add(tableau);
-        //pan.add(tableau, BorderLayout.CENTER);
-        f.getContentPane().add(tableau.getTableHeader(), BorderLayout.NORTH);
-        f.add(scrollPane, BorderLayout.SOUTH);
-        //f.getContentPane().add(tableau, BorderLayout.CENTER);
-
-
-        f.add(panH, BorderLayout.NORTH);
-        f.add(panb, BorderLayout.SOUTH);
-
-
-
-
-        JLabel label = new JLabel();
-        f.add(label);
-        label.setText("Tech-up");
-        label.setVerticalAlignment(JLabel.NORTH);
-        label.setHorizontalAlignment(JLabel.LEFT);
-        label.setForeground(new Color(0xF));//color of test
-        label.setFont(new Font("", Font.PLAIN,20));//size of the text
-        label.setBackground(Color.orange);
-        label.setOpaque(true);
-        label.setBorder(label.getBorder());
-        label.setBounds(0,0,600,200);
-        panH.add(label,BorderLayout.NORTH);
     }
 }
+
