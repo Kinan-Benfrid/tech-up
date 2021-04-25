bd5b95d27be9c5d74ca9c1feda3a8be4dd43bf16
AcilKh <acilpro@outlook.fr>
2021-04-25 03:23:52 +0200

    added FirstPageRentCriteria redirection page

--- client/src/main/java/episen/si/ing1/pds/client/view/HomePageRentView.java
+++ client/src/main/java/episen/si/ing1/pds/client/view/HomePageRentView.java
@@ -7,28 +7,10 @@ import java.awt.event.ActionListener;
 
 public class HomePageRentView extends CommonFrame implements ActionListener {
     private final JButton bouton1;
-    //private final JPanel pan1;
-   // private final JPanel pan2;
+
 
 
     public HomePageRentView() {
-
-      /*  this.setTitle("Louer des espaces");
-        this.setSize(200, 200);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        pan1 = new JPanel();
-        pan1.setBackground(Color.PINK);
-        pan1.setPreferredSize(new Dimension(500,70));
-        this.add(pan1, BorderLayout.PAGE_START);
-
-        pan2 = new JPanel();
-        pan2.setBackground(Color.PINK);
-        pan2.setPreferredSize(new Dimension(500,70));
-        this.add(pan2, BorderLayout.PAGE_END);
-
-       */
 
         JPanel panel = new JPanel();
         JLabel label1 = new JLabel("Bienvenue dans votre espace de réservation !");
@@ -39,32 +21,20 @@ public class HomePageRentView extends CommonFrame implements ActionListener {
         bouton1.setBackground(new Color(111,174,143));
         bouton1.setForeground(Color.black);
         this.add(bouton1);
-
         getContentPane().add(panel);
-
         bouton1.addActionListener(this);
-
-
-
 
     }
 
     public void actionPerformed(ActionEvent e) {
-        //bouton1.setText("Reserver des espaces");
-
-
         FirstPageRentCriteria fen = new FirstPageRentCriteria();
+        fen.setVisible(true);
         this.dispose();
-
     }
 
-
     public static void main(String[] args) {
-
         HomePageRentView hprm = new HomePageRentView();
         hprm.setVisible(true);
-
-
     }
 }
 
