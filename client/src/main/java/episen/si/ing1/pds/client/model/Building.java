90414d8ad266cb6650fa43f61c826e2ea743afd2
Mehdi Koumad <koumad094mehdi@gmail.com>
2021-04-28 01:14:44 +0200

    Add a Building class to store building_id

--- client/src/main/java/episen/si/ing1/pds/client/model/Building.java
+++ client/src/main/java/episen/si/ing1/pds/client/model/Building.java
@@ -1,4 +1,17 @@
 package episen.si.ing1.pds.client.model;
 
 public class Building {
+    private static int building_id = 1;
+
+
+
+    public static int getBuiling_id(){
+        return building_id;
+    }
+
+    public static void setBuilding_id(int building_id){
+        Building.building_id = building_id;
+    }
+
+
 }
