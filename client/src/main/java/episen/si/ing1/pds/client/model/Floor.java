9e84312519c760ba377e74caa89abae0ea85bbf9
Mehdi Koumad <koumad094mehdi@gmail.com>
2021-04-28 03:01:04 +0200

    Refactor

--- client/src/main/java/episen/si/ing1/pds/client/model/Floor.java
+++ client/src/main/java/episen/si/ing1/pds/client/model/Floor.java
@@ -4,11 +4,11 @@ public class Floor {
 
     private static int floor_id = 1;
 
-    public static int getBuiling_id(){
+    public static int getFloor_id(){
         return floor_id;
     }
 
-    public static void setBuilding_id(int building_id){
+    public static void setFloor_id(int building_id){
         Floor.floor_id = building_id;
     }
 }
