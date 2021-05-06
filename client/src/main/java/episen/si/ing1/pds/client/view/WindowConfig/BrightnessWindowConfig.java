b976eccf39e746fce85b8b1a3051c14fdb487b3c
Stephanie Ndongmo <nguimdofany@mail.com>
2021-05-06 07:03:11 +0200

    End modification in brightness

--- client/src/main/java/episen/si/ing1/pds/client/view/WindowConfig/BrightnessWindowConfig.java
+++ client/src/main/java/episen/si/ing1/pds/client/view/WindowConfig/BrightnessWindowConfig.java
@@ -15,136 +15,130 @@ import java.util.Map;
 import java.util.Vector;
 
 public class BrightnessWindowConfig extends CommonFrame implements ActionListener {
-
-    private JButton bconf, bv, bs, br;
+//declaration of variable
+    private JButton bconfiguration, bvalider, bsortir, bretour;
     private final SocketUtility socketUtility = new SocketUtility();
     private static final long serialVersionUID = 1L;
-    private JPanel p;
-    private JTextField luminterne,lumiexterne,pteinte;
-    ;
-
+    private JPanel panel;
+    private JTextField luminterne,lumiexterne;
     private JLabel  labelluminterne,labellumiexterne,labelluminterneecoute,labellumiexterneecoute;
-    private Integer InsertTemp;
+
 
     public BrightnessWindowConfig() {
-        p = new JPanel();
-        this.add(p);
-        p.setLayout(null);
+        panel = new JPanel();
+        this.add(panel);
+        panel.setLayout(null);
 
-        labelluminterne = new JLabel("Luminosite interieure");
+//label textfiel for insde brightness
+        labelluminterne = new JLabel("Luminosite interieure_entier");
         labelluminterne.setFont(new Font("Arial", Font.PLAIN, 18));
-        labelluminterne.setBounds(58, 150, 210, 29);
-        p.add(labelluminterne);
+        labelluminterne.setBounds(58, 150, 400, 29);
+        panel.add(labelluminterne);
 
         labelluminterneecoute = new JLabel("");
         labelluminterneecoute.setFont(new Font("Arial", Font.PLAIN, 25));
         labelluminterneecoute.setBounds(575, 150, 210, 29);
-        p.add(labelluminterneecoute);
-
+        panel.add(labelluminterneecoute);
 
         luminterne = new JTextField();
         luminterne.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        luminterne.setBounds(314, 150, 228, 40);
-        p.add(luminterne);
+        luminterne.setBounds(334, 150, 228, 40);
+        panel.add(luminterne);
         luminterne.setColumns(10);
-        luminterne.addActionListener(new ActionListener() {
-            @Override
+        luminterne.addActionListener(new ActionListener() {//action listenin jetxfiel, if we put enter, it print the value
             public void actionPerformed(ActionEvent e) {
                 String input2 = luminterne.getText();
                 labelluminterneecoute.setText(input2);
             }
         });
 
-
-        labellumiexterne= new JLabel("Luminosite exterieure(lux)");
+        labellumiexterne= new JLabel("Luminosite exterieure_entier(lux)");
         labellumiexterne.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        labellumiexterne.setBounds(58, 201, 220, 29);
-        p.add(labellumiexterne);
+        labellumiexterne.setBounds(58, 201, 400, 29);
+        panel.add(labellumiexterne);
 
         labellumiexterneecoute= new JLabel("");
         labellumiexterneecoute.setFont(new Font("Tahoma", Font.PLAIN, 25));
         labellumiexterneecoute.setBounds(575, 201, 210, 29);
-        p.add(labellumiexterneecoute);
+        panel.add(labellumiexterneecoute);
 
         lumiexterne = new JTextField();
         lumiexterne.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        lumiexterne.setBounds(314, 201, 228, 40);
-        p.add(lumiexterne);
+        lumiexterne.setBounds(334, 201, 228, 40);
+        panel.add(lumiexterne);
         lumiexterne.setColumns(10);
-        lumiexterne.addActionListener(new ActionListener() {
-            @Override
+        lumiexterne.addActionListener(new ActionListener() { //action listenin jetxfiel, if we put enter, it print the value
             public void actionPerformed(ActionEvent e) {
             String input = lumiexterne.getText();
                 labellumiexterneecoute.setText(input);
-
             }
         });
-        //tempextfiel.getActionListeners(btempextfielid);
 
+        bconfiguration = new JButton("CONFIGURATION DE LA LUMIERE");
+        bconfiguration.setBounds(250,20,450,50);
+        bconfiguration.setBackground(new Color(223,175,44));
+        bconfiguration.setFont(bconfiguration.getFont().deriveFont(15.0f));
+        panel.add(bconfiguration);
 
+//declaration of button
+        bvalider = new JButton("Valider");
+        bvalider.setBounds(800,380,92,25);
+        panel.add(bvalider);
+        bvalider.addActionListener(this);
 
-        bconf = new JButton("CONFIGURATION DE LA LUMIERE");
-        bconf.setBounds(250,20,450,50);
-        bconf.setBackground(new Color(223,175,44));
-        bconf.setFont(bconf.getFont().deriveFont(15.0f));
-        p.add(bconf);
+        bsortir = new JButton("Sortir");
+        bsortir.setBounds(800,450,92,25);
+        panel.add(bsortir);
+        bsortir.addActionListener(this);
 
-        bv = new JButton("Valider");
-        bv.setBounds(800,380,92,25);
-        p.add(bv);
-        bv.addActionListener(this);
-
-        bs = new JButton("Sortir");
-        bs.setBounds(800,450,92,25);
-        p.add(bs);
-        bs.addActionListener(this);
-
-
-
-        /*br = new JButton("Retour");
-        br.setBounds(10,20,110,25);
-        p.add(br);
-        br.addActionListener(this);*/
+        bretour = new JButton("Retour");
+        bretour.setBounds(10,20,110,25);
+        panel.add(bretour);
+        bretour.addActionListener(this);
 
     }
-
         public void actionPerformed(ActionEvent eb) {
             Object source = eb.getSource();
-            if(source == bs){
+            if(source == bsortir){
                 this.dispose();
                 PageOfConfigWindow bw = new PageOfConfigWindow();
                 bw.setVisible(true);
             }
-            if(source == bv){
+            if(source == bretour){
+                this.dispose();
+                TemperatureWindowConfig bw = new TemperatureWindowConfig();
+                bw.setVisible(true);
+            }
+            if(source == bvalider){
 
                 String vl6 = lumiexterne.getText();
                 int v6_pars = Integer.parseInt(vl6);
-                if( v6_pars > 100){
-                    JOptionPane.showMessageDialog(lumiexterne,"Attention ! La luminosité exterieure doit être inferieure à 100", "Avertissement", JOptionPane.WARNING_MESSAGE);
+                if( v6_pars > 50 || v6_pars < 0){
+                    JOptionPane.showMessageDialog(lumiexterne,"La luminosité Exterieure doit être comprise entre 0 & 50 lux", "ERREUR", JOptionPane.ERROR_MESSAGE);
                 }
 
                 String vl7 = luminterne.getText();
                 int v7_pars = Integer.parseInt(vl7);
-                if( v7_pars > 15){
-                    JOptionPane.showMessageDialog(luminterne,"Attention ! La luminosité interieure doit être entre 0 et 15", "Avertissement", JOptionPane.WARNING_MESSAGE);
+                if( v7_pars > 15 || v7_pars < 0){
+                    JOptionPane.showMessageDialog(luminterne,"La luminosité Interieure doit être comprise entre 0 & 50 lux", "ERREUR", JOptionPane.ERROR_MESSAGE);
                 }
+                else JOptionPane.showMessageDialog(luminterne,"Ok", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
 
-                if (0 < v7_pars && v7_pars< 15 ){
+                if(( v7_pars < 15 && v7_pars > 0) &&( v6_pars < 50 && v6_pars > 0)){
                 RequestSocket request = new RequestSocket();
                 request.setRequest("lum");
                 Map<String, Object> data = new HashMap<>();
-                //data.put("id_measure", Measured.getid_measure());
                 data.put("lumminosite_exterieure", v6_pars);
                 data.put("luminosite_interieure", v7_pars);
                 System.out.println(data);
                 request.setData(data);
+                System.out.println(data);
 
                 ResponseSocket response2 = socketUtility.sendRequest(request);
-               // java.util.List<Map> valeurTempi = (List<Map>) response2.getData();
+        }
+        }
+        }
 
-            }
-        }
-        }
 
     public static void main (String[]args){
         BrightnessWindowConfig bw = new BrightnessWindowConfig();
