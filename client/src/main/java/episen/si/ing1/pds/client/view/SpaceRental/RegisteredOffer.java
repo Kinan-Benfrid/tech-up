86fc748f597d388cb45323f307ae3765e44e8f90
AcilKh <acilpro@outlook.fr>
2021-05-06 07:37:01 +0200

    Addition of the future page which summarizes the selected offers

--- client/src/main/java/episen/si/ing1/pds/client/view/SpaceRental/RegisteredOffer.java
+++ client/src/main/java/episen/si/ing1/pds/client/view/SpaceRental/RegisteredOffer.java
@@ -1,21 +1,38 @@
 package episen.si.ing1.pds.client.view.SpaceRental;
 
 import episen.si.ing1.pds.client.view.CommonFrame;
+import episen.si.ing1.pds.client.view.HomePageView;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class RegisteredOffer extends Component {
+public class RegisteredOffer extends CommonFrame {
 
     public RegisteredOffer(){
 
         JOptionPane d = new JOptionPane();
-        int retour = JOptionPane.showConfirmDialog(this, "le message",
+        int boite = JOptionPane.showConfirmDialog(this, "Votre offre a bien été enregistrée",
                 "le titre", JOptionPane.OK_CANCEL_OPTION);
+
+
+        if ((boite == JOptionPane.OK_OPTION)){
+            this.dispose();
+            HomePageView hpm = new HomePageView();
+            hpm.setVisible(true);
+            this.setVisible(false);
+
+        }
+        else {
+            this.dispose();
+            FirstPageSelectionOffers fpso = new FirstPageSelectionOffers();
+            fpso.setVisible(true);
+        }
     }
 
 
     public static void main(String[] args) {
+        RegisteredOffer ro = new RegisteredOffer();
+        ro.setVisible(false);
 
     }
 
